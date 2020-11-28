d343039a4101c45e4325eba5a8d0e805fbe64aaa
Zach Arnold <me@zacharnold.org>
2020-11-27 19:49:44 -0600

    add aks

---
+++ test/aks/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  features {}
+  version = "=2.37.0"
+}
+
+terraform {
+  required_version = "0.13.0"
+}
+
+resource "azurerm_resource_group" "example" {
+  name = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name = "example-aks1"
+  location = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix = "exampleaks1"
+  sku_tier = "Paid"
+  default_node_pool {
+    name = "default"
+    node_count = 5
+    vm_size = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+}
